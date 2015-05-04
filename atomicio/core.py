b8326867a19fffc6a49a01079f008775362e504f
Eugene Eeo <packwolf58@gmail.com>
2015-05-04 20:54:01 +0800

    update docs [skip ci]

--- atomicio/core.py
+++ atomicio/core.py
@@ -1,26 +1,57 @@
+"""
+    atomicio.core
+    ~~~~~~~~~~~~~
+
+    Implements the low level object API.
+"""
+
 import os
 from contextlib import contextmanager
 from tempfile import NamedTemporaryFile
 
 
 class AtomicWriter(object):
+    """
+    Helper class for performing atomic writes.
+
+    :param path: Path to the file.
+    :param mode: Mode to open the file with.
+    """
+
     def __init__(self, path, mode):
         self.path = path
         self.mode = mode
 
     def commit(self, writer):
+        """
+        Commits the changes atomically (if it
+        fails then no changes are committed at
+        all) by renaming the temporary file.
+        """
         writer.flush()
         os.rename(writer.name,
                   self.path)
 
     def rollback(self, writer):
+        """
+        Cleans up the temporary resources.
+        """
         os.unlink(writer.name)
 
     def get_stream(self):
+        """
+        Get a temporary file to use.
+        """
         return NamedTemporaryFile(delete=False, mode=self.mode)
 
     @contextmanager
     def context(self):
+        """
+        Context manager that opens the temporary file
+        and yields it to the caller. Changes are then
+        committed if no exceptions are raised or else
+        they are simply dropped.
+        """
         with self.get_stream() as writer:
             try:
                 yield writer
